ac8f9c070e9f7cc66d2154f9ac1c0d00898e8dba
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-02-05 21:43:27 +0100

    Add project files.

---
+++ Uppgift 4 list inl/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Uppgift_4_Listor_inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] heheheha = new string[]{"hej", "nej", "ja"};
+            bool svar = FinnsTecknetIAllaSträngar(heheheha, 'j');
+            Console.WriteLine(svar);
+
+            svar = FinnsTecknetIAllaSträngar(heheheha, 'e');
+            Console.WriteLine(svar);
+        }
+        static bool FinnsTecknetIAllaSträngar(string[] args, char tecken)
+        {
+
+            foreach (string teststärng in args)
+            {
+                if (!Finnstäckenisträng(teststärng, tecken))
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        static bool Finnstäckenisträng(string teststräng, char tecken) 
+        {
+            foreach (char teckenisträng in teststräng)
+            {
+                if (teckenisträng == tecken)
+                {
+                    return true;
+
+                }
+            }
+            return false;
+        }
+
+    }
+}
